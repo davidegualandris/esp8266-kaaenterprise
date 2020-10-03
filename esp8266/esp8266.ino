564cbb55c9075a0ca0238c9f8a0444239e6174b9
Davide Gualandris <davide.gualandris969@gmail.com>
2020-10-03 17:57:09 +0200

    Actual sensors added

--- esp8266/esp8266.ino
+++ esp8266/esp8266.ino
@@ -1,12 +1,13 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
+#include <DHT.h>
 
 const char* ssid = "Guala's iPhone";        // WiFi name
 const char* password = "davide123";    // WiFi password
 const char* mqtt_server = "mqtt.cloud.kaaiot.com";
-const String TOKEN = "esp8266";        // Endpoint token - you get (or specify) it during device provisioning
-const String APP_VERSION = "bsbejo2ikfmmfgrj8iug-v1";  // Application version - you specify it during device provisioning
+const String TOKEN = "esp8266_2";        // Endpoint token - you get (or specify) it during device provisioning
+const String APP_VERSION = "btngtro547tsntf25rtg-v1";  // Application version - you specify it during device provisioning
 
 const unsigned long fiveSeconds = 1 * 5 * 1000UL;
 static unsigned long lastPublish = 0 - fiveSeconds;
@@ -14,7 +15,16 @@ static unsigned long lastPublish = 0 - fiveSeconds;
 WiFiClient espClient;
 PubSubClient client(espClient);
 
+// DHT sensor
+#define DHTPIN D1        // sensor I/O pin, eg. D1 (D0 and D4 are already used by board LEDs)
+#define DHTTYPE DHT11    // sensor type DHT 11 
+
+// Initialize DHT sensor
+DHT dht = DHT(DHTPIN, DHTTYPE);
+
 void setup() {
+  dht.begin();
+  
   Serial.begin(115200);
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
@@ -30,11 +40,32 @@ void loop() {
   unsigned long now = millis();
   if (now - lastPublish >= fiveSeconds) {
     lastPublish += fiveSeconds;
+    
+    // read temperature and humidity takes about 250 milliseconds!
+    float h = dht.readHumidity();     // humidity percentage
+    float t = dht.readTemperature();  // temperature Celsius
+
+    // check if everything is ok
+    if (isnan(h) || isnan(t)) {  // readings failed, skip
+      Serial.println(F("Failed to read from DHT sensor!"));
+      return;
+    }
+
+    // read gas level
+    float g = analogRead(A0); // gas level in %
+
+    // check if everything is ok, again
+    if (isnan(g)){
+      Serial.println(F("Failed to read from MQ-5 sensor!"));
+      return;
+    }
+    
     DynamicJsonDocument telemetry(1023);
     telemetry.createNestedObject();
-    telemetry[0]["temperature"] = random(18, 23);
-    telemetry[0]["humidity"] = random(40, 60);
-    telemetry[0]["co2"] = random(900, 1200);
+
+    telemetry[0]["temperature"] = t;
+    telemetry[0]["humidity"] = h;
+    telemetry[0]["co2"] = g;
 
     String topic = "kp1/" + APP_VERSION + "/dcx/" + TOKEN + "/json";
     client.publish(topic.c_str(), telemetry.as<String>().c_str());
@@ -52,7 +83,12 @@ void callback(char* topic, byte* payload, unsigned int length) {
 
   DynamicJsonDocument commandResponse(1023);
   for (int i = 0; i < json_var.size(); i++) {
+
     unsigned int command_id = json_var[i]["id"].as<unsigned int>();
+    const char* pay = json_var[i]["payload"].as<char*>();
+    Serial.println("command id " + String(command_id));
+    //Serial.printf("\nJSON: %s\n", pay.c_str());
+    
     commandResponse.createNestedObject();
     commandResponse[i]["id"] = command_id;
     commandResponse[i]["statusCode"] = 200;
@@ -104,7 +140,8 @@ void reconnect() {
 }
 
 void subscribeToCommand() {
-  String topic = "kp1/" + APP_VERSION + "/cex/" + TOKEN + "/command/SWITCH/status";
+  String topic_def = "kp1/" + APP_VERSION + "/cex/" + TOKEN + "/command/";
+  String topic = topic_def + "SWITCH";
   client.subscribe(topic.c_str());
   Serial.println("Subscribed on topic: " + topic);
 }
